a1846c69202e39594f0d161e1b50a96b3395a193
Tim Paduraru <timopaduraru@gmail.com>
2018-12-14 22:05:13 -0800

    both buttons in one window thanks to gtk::Box

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,24 @@ fn main() {
         return;
     }
 
-    let window1 = Window::new(WindowType::Toplevel);
-    window1.set_title("First GTK+ Program");
-    window1.set_default_size(350, 70);
-    let window2 = Window::new(WindowType::Toplevel);
-    window2.set_title("First GTK+ Program");
-    window2.set_default_size(350, 70);
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("First GTK+ Program");
+    window.set_default_size(350, 70);
     
     let button1 = Button::new_with_label("Click me!");
     let button2 = Button::new_with_label("No! Click me!");
-    window1.add(&button1);
-    window2.add(&button2);
-    window1.show_all();
-    window2.show_all();
+    
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    v_box.pack_start(&button1, false, false, 3);
+    v_box.pack_start(&button2, false, false, 3);
+    window.add(&v_box);
+    window.show_all();
 
-    window1.connect_delete_event(|_, _| {
+    window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
-    window2.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
+
 
     button1.connect_clicked(|_| {
         println!("Clicked!");
